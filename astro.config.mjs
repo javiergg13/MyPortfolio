1dcbc5deee3e67869defe3866cd241b7d500fb91
javiergg13 <javiergg13@educastur.es>
2024-06-10 22:14:13 +0200

    i18n, media querys

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   i18n: {
     defaultLocale: "es",
     locales: ['es', 'en'],
